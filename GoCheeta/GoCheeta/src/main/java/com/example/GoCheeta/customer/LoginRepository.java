e1c75a52891d301e6c14f319b4e23b1c739e1728
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2022-08-19 15:16:19 +0530

    Delete GoCheeta directory

--- GoCheeta/GoCheeta/src/main/java/com/example/GoCheeta/customer/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.GoCheeta.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-    @Repository
-    public interface LoginRepository extends JpaRepository<Customer, Long> {
-        Customer findByEmailAndPassword(String email, String password);
-}
