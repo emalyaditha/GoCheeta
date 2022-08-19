3108ed2a72e8c890e9b34b57839ff6fe0179b248
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2022-08-19 15:15:35 +0530

    Add files via upload

---
+++ GoCheeta/GoCheeta/src/main/java/com/example/GoCheeta/customer/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.GoCheeta.customer;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+    public long countById(Integer id);
+}
