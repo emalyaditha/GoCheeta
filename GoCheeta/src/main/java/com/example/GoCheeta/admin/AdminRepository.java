d9350592b673be86207af5c47a2fc4e435cd396f
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2022-08-22 01:12:11 +0530

    Delete GoCheeta directory

--- GoCheeta/src/main/java/com/example/GoCheeta/admin/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.GoCheeta.admin;
-
-import com.example.GoCheeta.customer.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminRepository extends CrudRepository<Customer, Integer> {
-    public long countById(Integer id);
-}
