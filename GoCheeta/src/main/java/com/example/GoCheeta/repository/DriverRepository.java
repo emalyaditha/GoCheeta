e9472dfe80da2305a61a883f1eb0148fbe5747de
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2022-08-22 22:13:14 +0530

    Delete GoCheeta directory

--- GoCheeta/src/main/java/com/example/GoCheeta/repository/DriverRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.GoCheeta.repository;
-
-import com.example.GoCheeta.model.Driver;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DriverRepository extends CrudRepository<Driver, Integer> {
-    public long countById(Integer id);
-}
