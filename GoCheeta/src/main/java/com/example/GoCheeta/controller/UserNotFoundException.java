07cc4e6c83f3d5575df25716897d5dc7c15c0640
Emal Yaditha <93852210+emalyaditha@users.noreply.github.com>
2022-08-26 13:07:42 +0530

    Delete GoCheeta directory

--- GoCheeta/src/main/java/com/example/GoCheeta/controller/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.GoCheeta.controller;
-
-public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
